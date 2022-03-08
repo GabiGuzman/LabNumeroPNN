a5a2dba01283b55ad047f110e2acae33b9977ded
Guzman <Guzman@DESKTOP-29GVUO5>
2022-03-08 10:43:47 -0600

    Agregar archivos de proyecto.

---
+++ NumeroPNN/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Double
+        a = TextBox1.Text
+        If (a < 0) Then
+            TextBox2.Text = "Negativo"
+        End If
+
+        If (a > 0) Then
+            TextBox2.Text = "Positivo"
+        End If
+
+        If (a = 0) Then
+            TextBox2.Text = "Nulo"
+        End If
+
+    End Sub
+End Class
